e475bd183e4a97f138638ec45eefda795e50b1e0
Melissa Schwartz <melissjs@gmail.com>
2017-09-25 17:14:01 -0700

    all

--- src/components/login-form/login-form.ts
+++ src/components/login-form/login-form.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { NavController, NavParams, ToastController } from 'ionic-angular';
-import { AngularFireAuth } from 'angularfire2/auth';
+// import { AngularFireAuth } from 'angularfire2/auth';
 import { Account } from '../../models/account/account.interface';
 import { LoginResponse } from '../../models/login/login-response.interface';
+import { AuthService } from '../../providers/auth/auth.service';
 
 @Component({
   selector: 'login-form',
@@ -13,7 +14,7 @@ export class LoginFormComponent {
   account = {} as Account;
   @Output() loginStatus: EventEmitter<LoginResponse>;
 
-  constructor(private toast: ToastController, private navCtrl: NavController, private navParams: NavParams, private afAuth: AngularFireAuth) {
+  constructor(private toast: ToastController, private navCtrl: NavController, private navParams: NavParams, private auth: AuthService) {
     this.loginStatus =  new EventEmitter<LoginResponse>();
   }
 
@@ -22,39 +23,46 @@ export class LoginFormComponent {
   }
 
   async register() {
-    try {
-      const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password);
-      this.toast.create({
-        message: 'User created',
-        duration: 3000
-      }).present();
-      console.log(result);
-    }
-    catch (e) {
-      console.error(e);
-      this.toast.create({
-        message: e.message,
-        duration: 3000
-      }).present();
-    }
+    const loginResponse = await this.auth.register(this.account);
+    this.loginStatus.emit(loginResponse);
   }
+  // async register() {
+  //   try {
+  //     const result = await this.afAuth.auth.createUserWithEmailAndPassword(this.account.email, this.account.password);
+  //     this.toast.create({
+  //       message: 'User created',
+  //       duration: 3000
+  //     }).present();
+  //     console.log(result);
+  //     this.navCtrl.setRoot('ProfilePage');
+  //   }
+  //   catch (e) {
+  //     console.error(e);
+  //     this.toast.create({
+  //       message: e.message,
+  //       duration: 3000
+  //     }).present();
+  //   }
+  // }
 
 
   async login() {
-    try {
-      const result: LoginResponse = 
-      { result: await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password)}
-      this.loginStatus.emit(result);
-      console.log('result', result);
-      console.log('result.error', result.error);
-    }
-    catch (e) {
-      const error: LoginResponse = {
-        error: e
-      }
-      this.loginStatus.emit(error);
-      // console.error('login resp from component', error.error.message);
-    }
+    const loginResponse = await this.auth.signInWithEmailAndPassword(this.account);
+    this.loginStatus.emit(loginResponse);
+    // try {
+    //   const result: LoginResponse = 
+    //   { result: await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password)}
+    //   this.loginStatus.emit(result);
+    //   console.log('result', result);
+    //   console.log('result.error', result.error);
+    // }
+    // catch (e) {
+    //   const error: LoginResponse = {
+    //     error: e
+    //   }
+    //   this.loginStatus.emit(error);
+    //   // console.error('login resp from component', error.error.message);
+    // }
   }
 
 }
---
+++ src/providers/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Account } from '../../models/account/account.interface'
+import { LoginResponse } from '../../models/login/login-response.interface'
+// import { Http } from '@angular/http';
+// import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+    async register(account: Account) {
+    try {
+      return <LoginResponse> {
+        result: await this.afAuth.auth.createUserWithEmailAndPassword(account.email, account.password)
+      }
+    }
+    catch (e) {      
+      return <LoginResponse> {
+      error: e
+    }
+    }
+  }
+
+  async signInWithEmailAndPassword(account: Account) {
+    try {
+      return <LoginResponse> {
+        result: await this.afAuth.auth.signInWithEmailAndPassword(account.email, account.password)
+      } 
+    }
+    catch (e) {
+      return <LoginResponse> {
+        error: e
+      }
+    }
+  }
+
+}
