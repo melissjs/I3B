284f39d2b538e5e57e574c509d2d92fdb1406ff2
Melissa Schwartz <melissjs@gmail.com>
2017-10-29 13:30:13 -0700

    all

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Profile } from '../../models/profile/profile.interface';
+import { AuthService } from '../../providers/auth/auth.service';
 
 @IonicPage()
 @Component({
@@ -11,11 +12,16 @@ export class ProfilePage {
 
   existingProfile = {} as Profile;
 
-  constructor(private navCtrl: NavController, private navParams: NavParams) {
+  constructor(private auth: AuthService, private navCtrl: NavController, private navParams: NavParams) {
   }
 
   getExistingProfile(profile: Profile) {
     this.existingProfile = profile;
+  }
+
+  signOut() {
+    this.auth.signOut();
+    this.navCtrl.setRoot('LoginPage');
   }
 
   navigateToEditProfilePage() {
--- src/providers/auth/auth.service.ts
+++ src/providers/auth/auth.service.ts
@@ -44,4 +44,8 @@ export class AuthService {
     }
   }
 
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+
 }
