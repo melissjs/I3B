2c955a5fc7ccd907541f2430c3f1c44ba784eadc
Melissa Schwartz <melissjs@gmail.com>
2017-11-03 17:38:09 -0700

    fixed

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// https://animal-rescue-mobile.firebaseapp.com
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
@@ -13,8 +15,8 @@ exports.addUserMessages = functions.database.ref(`/messages/{messageId}`)
     const messageKey = event.data.key;
     const messageValue = event.data.val();
 
-    admin.database().ref(`/user-messages/${messageValue/userFromId}/${messageValue/userToId}`)
+    admin.database().ref(`/user-messages/${messageValue.userFromId}/${messageValue.userToId}`)
     .child(messageKey).set(1);
-    admin.database().ref(`/user-messages/${messageValue/userToId}/${messageValue/userFromId}`)
+    admin.database().ref(`/user-messages/${messageValue.userToId}/${messageValue.userFromId}`)
     .child(messageKey).set(1);
 })
